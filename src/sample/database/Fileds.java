f4c250df24ce31d3d7f8432c20bd69d7c6463af6
Festive <27864684+mash-reza@users.noreply.github.com>
2019-11-14 22:55:51 +0330

    created Fields class for database constant field names, added customer fields

---
+++ src/sample/database/Fileds.java
@@ -0,0 +1,8 @@
+package sample.database;
+
+public class Fileds {
+    public static final String id = "id";
+    public static final String name = "name";
+    public static final String charge = "charge";
+    public static final String phone = "phone";
+}
